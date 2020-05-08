6a105a97ce6e23f397a9f8d249408a1ae7858755
Luuk van Kooten <luukvankooten@gmail.com>
2020-05-08 12:37:16 +0200

    Scaffold plugin and add mc4wp-snippets as submodule

---
+++ wp-snippets.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wp Snippets
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wp-snippets
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Snippets
+ */
+
+// Your code starts here.
